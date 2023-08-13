5117797a19afc27facb2b05ebb197bce7f0e7a4d
Steven <steven@ceriously.com>
2023-08-13 15:04:02 -0400

    refactor

--- index.ts
+++ index.ts
@@ -1,19 +1,26 @@
-export const BULB_CONTROL_SERVICE = 0xffe5;
-export const BULB_CONTROL_CHARACTERISTIC = 0xffe9;
-
 export class MagicBlueBulb {
     private characteristic: BluetoothRemoteGATTCharacteristic;
-
+    private serviceId = 0xffe5;
+    private charId = 0xffe9;
+    
     constructor() {
     }
 
-    async connect() {
+    async init() {
         const device = await navigator.bluetooth.requestDevice({
-            filters: [{ services: [BULB_CONTROL_SERVICE] }]
+            filters: [{ services: [this.serviceId] }]
         });
+        await this.connect(device);
+    }
+
+    async connect(device: BluetoothDevice) {
         const gatt = await device.gatt.connect();
-        const service = await gatt.getPrimaryService(BULB_CONTROL_SERVICE);
-        this.characteristic = await service.getCharacteristic(BULB_CONTROL_CHARACTERISTIC);
+        const service = await gatt.getPrimaryService(this.serviceId);
+        this.characteristic = await service.getCharacteristic(this.charId);
+    }
+
+    write(bytes: number[]) {
+        return this.characteristic.writeValue(new Uint8Array(bytes));
     }
 
     disconnect() {
@@ -22,27 +29,22 @@ export class MagicBlueBulb {
     }
 
     setColor(red: number, green: number, blue: number) {
-        return this.characteristic.writeValue(
-            new Uint8Array([0x56, red, green, blue, 0x00, 0xf0, 0xaa]));
+        return this.write([0x56, red, green, blue, 0x00, 0xf0, 0xaa]);
     }
 
     setWarmWhite(brightness: number) {
-        return this.characteristic.writeValue(
-            new Uint8Array([0x56, 0, 0, 0, brightness, 0x0f, 0xaa]));
+        return this.write([0x56, 0, 0, 0, brightness, 0x0f, 0xaa]);
     }
 
     presetMode(preset: number, speed: number) {
-        return this.characteristic.writeValue(
-            new Uint8Array([0xbb, preset, speed, 0x44]));
+        return this.write([0xbb, preset, speed, 0x44]);
     }
 
     powerOn() {
-        return this.characteristic.writeValue(
-            new Uint8Array([0xcc, 0x23, 0x33]));
+        return this.write([0xcc, 0x23, 0x33]);
     }
 
     powerOff() {
-        return this.characteristic.writeValue(
-            new Uint8Array([0xcc, 0x24, 0x33]));
+        return this.write([0xcc, 0x24, 0x33]);
     }
 }
